f93561d346f6a4e69b73209ed7d9ed68a930ad4f
Montycarlo <joshua.brown@au.ey.com>
2016-05-02 10:05:15 +1000

    noting props to forward

--- src/lib/helpers/react/forms/nodes/DatePicker.hx
+++ src/lib/helpers/react/forms/nodes/DatePicker.hx
@@ -10,6 +10,38 @@ private typedef DatePickerProps = {
 	@optional public var formkey:String;
 	@optional public var label:String;
 	@optional public var bind:Field<Moment>;
+// Forwarded fields
+	@optional public var className:String;
+	@optional public var dateFormat:String;
+	@optional public var dateFormatCalendar:String;
+	@optional public var disabled:Bool;
+	@optional public var startDate:Dynamic;
+	@optional public var endDate:Dynamic;
+	@optional public var includeDates:Array<Dynamic>;
+	@optional public var excludeDates:Array<Dynamic>;
+	@optional public var filterDate:Dynamic->Bool;
+	@optional public var id:String;
+	@optional public var isClearable:Bool;
+	@optional public var locale:String;
+	@optional public var maxDate:Dynamic;
+	@optional public var minDate:Dynamic;
+	@optional public var name:String;
+	@optional public var onBlur:Void->Void;
+	@optional public var onChange:Moment->Void;
+	@optional public var onFocus:Void->Void;
+	@optional public var placeholderText:String;
+	@optional public var popoverAttachment:String;
+	@optional public var popovertargetAttachment:String;
+	@optional public var popoverTargetOffset:String;
+	@optional public var readOnly:Bool;
+	@optional public var renderCalendarTo:Dynamic;
+	@optoinal public var required:Bool;
+	@optional public var selected:Moment;
+	@optoinal public var showYearDropdown:Bool;
+	@optional public var tabIndex:Int;
+	@optional public var tetherConstraints:Array<Dynamic>;
+	@optional public var title:String;
+	@optoinal public var todayButton:String;
 }
 private class DatePickerState{
 	public var value:Field<Moment>;
